91bb0a971e9f86389fb7e855ee16b9d988211cb5
Olivier Scherrer <pode.fr@gmail.com>
2012-05-17 15:09:54 +0200

    suggestions server is now using connect

--- server.js
+++ server.js
@@ -3,38 +3,20 @@
  * Copyright(c) 2012 Ta•aut
  * MIT Licensed
  */
-var urlParse = require("url"),
-	app = require('http').createServer(function (req, res) {
-		
-	var url = urlParse.parse(req.url),
-		file = url.pathname != "/" ? url.pathname : '/index.html',
-		filetype = file.split(".").pop(),
-		types = {"html" : "text/html",
-				 "js" : "text/javascript",
-				 "css": "text/css",
-				 "png": "image/png"};
 
-  fs.readFile(__dirname + file,
-		  function (err, data) {
-		    if (err) {
-		      res.writeHead(500);
-		      return res.end('Error loading index.html');
-		    }
+// Required middleware
+var Connect = require("connect"),
+	http = require("http"),
+	io = require("socket.io"),
+	olives = require("olives"),
+	browserID = require("olives-browserid-handler");
 
-		    res.writeHead(200, { 'Content-Type': types[filetype] });
-		    res.end(data);
-		  });
-		}
-).listen(8000),
-io = require('socket.io').listen(app),
-fs = require('fs'),
-http = require('http');
+// Configuring the middleware
+var connect = Connect()
+	.use(Connect.static(__dirname + "/"));
 
-http.globalAgent.maxSockets = Infinity;
+var app = http.createServer(connect).listen(8000);
 
-var olives = require("olives"),
-BrowserID = require("olives-browserid-handler");
-// Register the instance of socket.io
-// This API will change until the final release
-olives.registerSocketIO(io);
-olives.handlers.set("BrowserID", BrowserID.handler)
+olives.registerSocketIO(io.listen(app));
+
+olives.handlers.set("BrowserID", browserID.handler);
