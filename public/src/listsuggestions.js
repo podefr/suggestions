55f844fdee2a7000c669e21095ce3eb1f634f415
Olivier Scherrer <pode.fr@gmail.com>
2012-10-21 23:05:41 +0100

    removed debugging code

--- public/src/listsuggestions.js
+++ public/src/listsuggestions.js
@@ -3,28 +3,28 @@
  * Copyright(c) 2012 Ta•aut
  * MIT Licensed
  */
-define("ListSuggestions", ["Olives/OObject", "CouchDBStore", "Olives/Model-plugin", "Olives/Event-plugin", "Config", "Services"], 
-		
+define("ListSuggestions", ["Olives/OObject", "CouchDBStore", "Olives/Model-plugin", "Olives/Event-plugin", "Config", "Services"],
+
 function (OObject, CouchDBStore, ModelPlugin, EventPlugin, Config, Services) {
-	
+
 	/**
 	 * Defines the list of suggestions UI.
 	 * It can be resynchronized to display different suggestions
 	 */
 	return function ListSuggestionsConstructor() {
-		
+
 		// The UI's model
 		var couchDBStore = new CouchDBStore(),
-		
+
 		// A listSuggestions UI that is based on a OObject
 		// The OObject will have the couchDBStore as model
 		listSuggestions = new OObject(couchDBStore);
-		
+
 		// Adding a Model plugin to listSuggestion UI to bind it's dom with it's model
 		listSuggestions.plugins.addAll({
 			"model": new ModelPlugin(listSuggestions.model, {
 				formatDate: function (date) {
-					this.innerHTML = new Date(date[0], date[1] , date[2]).toLocaleDateString();	
+					this.innerHTML = new Date(date[0], date[1] , date[2]).toLocaleDateString();
 				}
 			}),
 			"event": new EventPlugin(listSuggestions)
@@ -32,34 +32,31 @@ function (OObject, CouchDBStore, ModelPlugin, EventPlugin, Config, Services) {
 
 		// Set couchDBStore's transport
 		couchDBStore.setTransport(Config.get("Transport"));
-		
+
 		// Synchronize the store with the "id" view
 		listSuggestions.model.sync("suggestions", "list", "_view/id", {
 			descending: true
 		});
-		
+
 		// Make the dom alive
 		listSuggestions.alive(Config.get("listUI"));
-		
+
 		// Declare a list route for displaying the list
 		Services.routing.set("list", function () {
 			Services.screens.show("list");
 		});
-		
+
 		// Declare the list UI
 		Services.screens.add("list", listSuggestions);
-		
+
 		// The edit action for editing a suggestion
 		listSuggestions.edit = function (event, node) {
 			Services.routing.get("edit", couchDBStore.get(node.dataset["model_id"]).id);
 		};
 
-		cd = new CouchDBStore;
-		cd.setTransport(Config.get("Transport"));
-		
 		// And return the new UI
 		return listSuggestions;
 	};
-	
-	
+
+
 });
